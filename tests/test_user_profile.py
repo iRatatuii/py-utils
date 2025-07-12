c492e23dec1eb27f124cf63ee2548ea93afaf567
iratatuii <andreikarpenkin123@gmail.com>
2025-07-12 14:32:15 +0300

    test: add test for function collect_user_profile

---
+++ tests/test_user_profile.py
@@ -0,0 +1,15 @@
+from src.user_profile import collect_user_profile
+
+
+def test_collect_user_profile(monkeypatch):
+    inputs = iter(['Andy', '31', '83.0', '176', 'да', 'нет'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    
+    profile = collect_user_profile()
+    
+    assert profile['name'] == 'Andy'
+    assert profile["age"] == 31
+    assert profile["weight"] == 83.0
+    assert profile["height"] == 176
+    assert profile["is_active"] is True
+    assert profile["has_subscription"] is False
